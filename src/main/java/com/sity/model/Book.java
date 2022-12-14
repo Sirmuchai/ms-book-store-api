9c82faef93f672e80fab7f7781e00a87f12c1ec9
Sirmuchai <sirmemu@gmail.com>
2022-12-14 10:45:52 +0300

    Lombok depedencies added

--- src/main/java/com/sity/model/Book.java
+++ src/main/java/com/sity/model/Book.java
@@ -2,12 +2,20 @@ package com.sity.model;
 
 import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
 @Entity(name = "Book")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Book {
     @Id
     @SequenceGenerator(
@@ -43,88 +51,9 @@ public class Book {
     @JsonIgnoreProperties("book")
     private Set<Author> author;
    //No args constructor
-    public Book(){
-    }
+
     //Class constrcutor with variables
 
 
-    public Book(Integer id, String title, String category, Long isbn, boolean isPublished, Set<Publisher> publisher, Set<Author> author) {
-        this.id = id;
-        this.title = title;
-        this.category = category;
-        this.isbn = isbn;
-        this.isPublished = isPublished;
-        this.publisher = publisher;
-        this.author = author;
-    }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Long getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(Long isbn) {
-        this.isbn = isbn;
-    }
-
-    public boolean isPublished() {
-        return isPublished;
-    }
-
-    public void setPublished(boolean published) {
-        isPublished = published;
-    }
-
-    public Set<Publisher> getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(Set<Publisher> publisher) {
-        this.publisher = publisher;
-    }
-
-    public Set<Author> getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Set<Author> author) {
-        this.author = author;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", category='" + category + '\'' +
-                ", isbn=" + isbn +
-                ", isPublished=" + isPublished +
-                ", publisher=" + publisher +
-                ", author=" + author +
-                '}';
-    }
 }
